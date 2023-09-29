339785e84b76642aa1bdbdf0947b10d31b0af63c
yb2013 <yb2013@hw.ac.uk>
2023-09-29 10:22:09 +0100

    reset button added

--- Javascript/counter.js
+++ Javascript/counter.js
@@ -3,6 +3,7 @@ let count = 0;
 const counterValue = document.getElementById("counter-value");
 const incrementBtn = document.getElementById("increment-btn");
 const decrementBtn = document.getElementById("decrement-btn");
+const resetBtn = document.querySelector("#reset");
 
 incrementBtn.addEventListener('click', () => {
     count++;
@@ -13,3 +14,8 @@ decrementBtn.addEventListener('click', () => {
     count--;
     counterValue.innerHTML = count;
 });
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  counterValue.innerHTML = count;
+})
